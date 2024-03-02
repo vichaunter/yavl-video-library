29e216e2471335887d8b9faf6c4aca735aedb5f3
VicHaunter <vichaunter@gmail.com>
2024-03-02 15:57:18 +0100

    feat(videoLibrary): add sorting hability

---
+++ src/renderer/src/helpers.ts
@@ -0,0 +1,1 @@
+export const ascDesc = (asc: boolean) => (asc ? 'asc' : 'desc');
--- src/renderer/src/screens/HomeScreen.tsx
+++ src/renderer/src/screens/HomeScreen.tsx
@@ -1,35 +1,113 @@
-import { useEffect, useState } from 'react';
-import api from '../api/apiMedia';
+import { orderBy } from 'lodash';
+import { useMemo, useState } from 'react';
+import {
+  FaEye,
+  FaEyeSlash,
+  FaRegStar,
+  FaSortAlphaDown,
+  FaSortAlphaUpAlt,
+  FaStar,
+} from 'react-icons/fa';
+import styled from 'styled-components';
+import MediaModel from '../../../main/models/MovieModel';
 import MediaItem from '../components/MediaItem';
+import useFiles from '../hooks/useFiles';
 import useFolder from '../hooks/useFolder';
-import { Media as MediaType } from '../../../main/helpers';
-import useWatchedStore from '../store/useWatchedStore';
+import { ascDesc } from '../helpers';
+
+const Styled = styled.div`
+  .header {
+    display: flex;
+    justify-content: space-between;
+  }
+`;
+
+const SORTERS: Record<
+  string,
+  (items: MediaModel[], dir: 'asc' | 'desc') => MediaModel[]
+> = {
+  id: (items, dir) => orderBy(items, 'id', dir),
+  favorites: (items, dir) =>
+    dir === 'desc' ? items : orderBy(items, 'user.favorite', 'desc'),
+};
+const sortItems = (items: MediaModel[], sort: Record<string, boolean>) => {
+  let sortedItems = items;
+
+  // Loop through each sort option
+  for (const [key, value] of Object.entries(sort)) {
+    sortedItems = SORTERS[key](sortedItems, ascDesc(value));
+  }
+
+  return sortedItems;
+};
 
 const HomeScreen = () => {
-  const [files, setFiles] = useState<MediaType[]>([]);
+  const [sort, setSort] = useState<Record<string, boolean>>({
+    id: true,
+    favorites: false,
+  });
+  const [filter, setFilter] = useState<Record<string, boolean>>({
+    watched: true,
+  });
+
   const { current, openDialog } = useFolder();
 
-  useEffect(() => {
-    if (!current) return;
-    api.listFiles({ folder: current }).then((files) => setFiles(files));
-  }, [current]);
+  const { items, loading } = useFiles();
+
+  const toggleSort = (type: string) => {
+    const newSort = { ...sort, [type]: !sort[type] };
+    setSort(newSort);
+  };
+
+  const togglefilter = (type: string) => {
+    const newFilter = { ...filter, [type]: !filter[type] };
+    setFilter(newFilter);
+  };
+
+  const filteredItems = !filter.watched
+    ? items.filter((item) => !item.user?.watched)
+    : items;
+
+  const sortedItems = useMemo(() => {
+    let sortedItems = filteredItems;
+
+    // Loop through each sort option
+    for (const [key, value] of Object.entries(sort)) {
+      sortedItems = SORTERS[key](sortedItems, ascDesc(value));
+    }
+
+    return sortedItems;
+  }, [filteredItems, sort]);
 
   return (
-    <div>
-      <button onClick={openDialog}>{current}</button>
+    <Styled>
+      <div className="header">
+        <button onClick={openDialog}>{current}</button>
+        {loading && <div>Loading...</div>}
+        <div className="actions">
+          <button onClick={() => toggleSort('id')}>
+            {sort.id ? <FaSortAlphaDown /> : <FaSortAlphaUpAlt />}
+          </button>
+          <button onClick={() => toggleSort('favorites')}>
+            {sort.favorites ? <FaStar /> : <FaRegStar />}
+          </button>
+          <button onClick={() => togglefilter('watched')}>
+            {filter.watched ? <FaEye /> : <FaEyeSlash />}
+          </button>
+        </div>
+      </div>
       <div className="grid-container">
-        {files?.map((file) => {
+        {sortedItems?.map((file) => {
           return (
             <MediaItem
-              key={file.fullPath}
-              fullPath={file.fullPath}
-              fileName={file.name}
+              key={file.fileInfo?.data?.fullpath}
+              item={file}
               className="grid-item"
             />
           );
         })}
       </div>
-    </div>
+    </Styled>
   );
 };
 
